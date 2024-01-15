21498a66785bc27895b19bdc93d2f214137ad1ce
C0lgate <k0lesan2k00@gmail.com>
2024-01-15 13:33:18 +0300

    feat: implement quiz game

---
+++ hangman/js/game.js
@@ -0,0 +1,15 @@
+import listenerInterface from './listenerKeyboard.js';
+
+function startGame() {
+  const keyboard = document.querySelector('.keyboard');
+  const game = new listenerInterface(keyboard);
+
+  keyboard.addEventListener('click', game, {
+    passive: true
+  });
+  document.addEventListener('keyup', game, {
+    passive: true
+  });
+}
+
+document.addEventListener('DOMContentLoaded', startGame);
---
+++ hangman/js/listenerKeyboard.js
@@ -0,0 +1,103 @@
+export default class {
+  answerLength = document.querySelector('.answer').children.length;
+  correctLetters = 0;
+
+  constructor(keyboard) {
+    this.keyboard = keyboard;
+  }
+
+  handleEvent(event) {
+    switch (event.type) {
+      case 'click':
+        this.clickOnButton(event);
+        break;
+      case 'keyup':
+        this.clickOnKeyboard(event);
+        break;
+    }
+  }
+
+  finishGame() {
+    this.keyboard.removeEventListener('click', this);
+    document.removeEventListener('keyup', this);
+  }
+
+  plusCountAndShowLimb(answerHasLetter) {
+    if (this.correctLetters === this.answerLength) {
+      this.finishGame();
+      return;
+    }
+
+    if (answerHasLetter) {
+      return;
+    }
+
+    const countGuesses = document.querySelector('.quiz__guesses_counter');
+    const countIncorrectGuesses = +countGuesses.textContent[0];
+
+    countGuesses.textContent = `${countIncorrectGuesses + 1} / 6`;
+
+    document.querySelectorAll('.gallows__limb')[
+      countIncorrectGuesses
+    ].style.opacity = 1;
+
+    if (countIncorrectGuesses === 5) {
+      this.finishGame();
+    }
+  }
+
+  showLetterInAnswer(key) {
+    const answer = document.querySelector('.answer');
+    let answerHasLetter = false;
+
+    for (const element of answer.children) {
+      const letter = element.getAttribute('data-letter');
+
+      if (letter.toLowerCase() === key) {
+        element.classList.add('answer__item_visible');
+        element.textContent = letter;
+        answerHasLetter = true;
+        this.correctLetters++;
+      }
+    }
+
+    this.plusCountAndShowLimb(answerHasLetter);
+  }
+
+  clickOnButton(event) {
+    const button = event.target.closest('.keyboard__item');
+
+    if (!button || button.disabled) {
+      return;
+    }
+
+    button.disabled = true;
+    this.showLetterInAnswer(button.textContent.toLowerCase());
+  }
+
+  findButton(key) {
+    if (!/^[a-z]{1}$/.test(key)) {
+      return null;
+    }
+
+    for (const element of this.keyboard.children) {
+      if (element.textContent === key) {
+        return element;
+      }
+    }
+
+    return null;
+  }
+
+  clickOnKeyboard(event) {
+    const key = event.key.toLowerCase();
+    const button = this.findButton(key);
+
+    if (!button || button.disabled) {
+      return;
+    }
+
+    this.showLetterInAnswer(key);
+    button.disabled = true;
+  }
+}
