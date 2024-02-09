591a4e7e142342d1cdd6b45e7ef81233ec53efe9
C0lgate <k0lesan2k00@gmail.com>
2024-02-09 03:44:15 +0300

    feat: deploy project

---
+++ js/question-generator.js
@@ -0,0 +1,40 @@
+import data from './questions-and-answers.js';
+
+function generateAnswerBlock(answer) {
+  const answerBlock = document.querySelector('.answer');
+  const answerLength = answer.length;
+
+  answerBlock.innerHTML = '';
+
+  for (let index = 0; index < answerLength; index++) {
+    const letter = document.createElement('div');
+    letter.classList.add('answer__item');
+    letter.setAttribute('data-letter', answer[index]);
+
+    answerBlock.append(letter);
+  }
+}
+
+function generateRandomAnswer() {
+  const length = data.length;
+  const previousAnswer = sessionStorage.getItem('answer');
+  const randomIndex = () => Math.floor(Math.random() * length);
+  let questionAndAnswer = data[randomIndex()];
+
+  while (questionAndAnswer.answer === previousAnswer) {
+    questionAndAnswer = data[randomIndex()];
+  }
+
+  const { question, answer } = questionAndAnswer;
+
+  generateAnswerBlock(answer);
+
+  console.log(answer);
+
+  sessionStorage.setItem('answer', answer);
+  document.querySelector('.quiz__question').textContent = question;
+}
+
+generateRandomAnswer();
+
+export { generateRandomAnswer };
---
+++ js/questions-and-answers.js
@@ -0,0 +1,98 @@
+export default [
+  {
+    question: 'What is the capital of France?',
+    answer: 'Paris'
+  },
+  {
+    question: 'What is the largest planet in our solar system?',
+    answer: 'Jupiter'
+  },
+  {
+    question: 'What is gold called in chemistry?',
+    answer: 'Aurum'
+  },
+  {
+    question: 'What is the primary language spoken in Brazil?',
+    answer: 'Portuguese'
+  },
+  {
+    question: 'What is the currency of Japan?',
+    answer: 'Yen'
+  },
+  {
+    question: 'What is the capital of Australia?',
+    answer: 'Canberra'
+  },
+  {
+    question: 'What is the highest grossing film of all time?',
+    answer: 'Avengers'
+  },
+  {
+    question: 'What is the largest country in South America?',
+    answer: 'Brazil'
+  },
+  {
+    question: 'What is the capital of Italy?',
+    answer: 'Rome'
+  },
+  {
+    question: 'What is the deepest part of the ocean?',
+    answer: 'Mariana'
+  },
+  {
+    question: 'What is the official language of China?',
+    answer: 'Mandarin'
+  },
+  {
+    question: 'What is the currency of Canada?',
+    answer: 'Dollar'
+  },
+  {
+    question: 'What is the largest desert in the world?',
+    answer: 'Sahara'
+  },
+  {
+    question: 'What is the capital of Russia?',
+    answer: 'Moscow'
+  },
+  {
+    question: 'What is the highest mountain in North America?',
+    answer: 'McKinley'
+  },
+  {
+    question: 'What is the official language of India?',
+    answer: 'Hindi'
+  },
+  {
+    question: 'What is the currency of Mexico?',
+    answer: 'Peso'
+  },
+  {
+    question: 'What is the largest lake in Africa?',
+    answer: 'Victoria'
+  },
+  {
+    question: 'What is the capital of Spain?',
+    answer: 'Madrid'
+  },
+  {
+    question: 'What is the highest waterfall in the world?',
+    answer: 'Angel'
+  },
+  {
+    question: 'What is the official language of Germany?',
+    answer: 'German'
+  },
+  {
+    question: 'What is the currency of Brazil?',
+    answer: 'Real'
+  },
+  {
+    question: 'What is the largest island in the world?',
+    answer: 'Greenland'
+  },
+  {
+    question: 'What is the capital of South Africa?',
+    answer: 'Pretoria'
+  }
+];
